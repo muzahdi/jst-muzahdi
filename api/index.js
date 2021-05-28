54f895c40f92b02bd4a27430ac5e5cb892b0de0c
muzahdi <84892113+muzahdi@users.noreply.github.com>
2021-05-28 20:18:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,17 +16,45 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
+state = 0
 bot.onText(/\/menu/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `masukkan nilai i|v contohnya 9|9`
     );   
+    state  = 1
 });
+
+bot.on('message',(msg) => {
+    if(state == 1){
+        console.log(msg.text);
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+        [
+            parseFloat(req.params.i), // string to float
+            parseFloat(req.params.r)
+        ]
+    ).then((jres)=>{
+      bot.sendMessage(
+          msg.chat.id,
+         `nilai v yang diprediksi adalah s{jres[0]} volt`
+      );
+      bot.sendMessage(
+          msg.chat.id,
+         `nilai p yang diprediksi adalah s{jres[0]} watt`
+      );   
+    })
+    }else{
+        state = 0
+    }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
