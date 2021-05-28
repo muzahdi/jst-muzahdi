f1df235107bfdfbd0f2065dcfc46a740caa7e0e1
muzahdi <84892113+muzahdi@users.noreply.github.com>
2021-05-28 21:28:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -44,11 +44,11 @@ bot.on('message',(msg) => {
        ).then((jres)=>{
            bot.sendMessage(
                msg.chat.id,
-               `nilai v yang diprediksi adalah s{jres[0]} volt`
+               `nilai v yang diprediksi adalah ${jres[0]} volt`
            );
            bot.sendMessage(
                msg.chat.id,
-               `nilai p yang diprediksi adalah s{jres[0]} watt`
+               `nilai p yang diprediksi adalah ${jres[1]} watt`
            );   
         })
     }else{
